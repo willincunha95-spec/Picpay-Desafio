25bb69b19bb6856a4fbd4cf4f995fbb7c091a2fc
willia <willincunha95@gmail.com>
2026-02-12 10:44:06 -0300

    feat : Create Transaction entity

---
+++ picpaysimplificado/src/test/java/com/picpaysimplificado/picpaysimplificado/domain/transaction/Transaction.java
@@ -0,0 +1,36 @@
+package com.picpaysimplificado.picpaysimplificado.domain.transaction;
+
+import com.picpaysimplificado.picpaysimplificado.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+
+    @ManyToOne
+    @JoinColumn (name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+
+}
