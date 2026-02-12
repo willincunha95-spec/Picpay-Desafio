4c9126520ad55c7fc97c0ba6129014be30e3d8ec
willia <willincunha95@gmail.com>
2026-02-12 14:53:53 -0300

    feat: desafio PicPay Concluido

--- picpaysimplificado/src/test/java/com/picpaysimplificado/picpaysimplificado/controller/UserController.java
+++ picpaysimplificado/src/test/java/com/picpaysimplificado/picpaysimplificado/controller/UserController.java
@@ -9,11 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/users")
+@RestController()
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
